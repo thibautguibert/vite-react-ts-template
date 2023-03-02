4b8c365b768b9cbd87c1615418b033bdeb2fa3a9
Thibaut Guibert <t.guibert@clear-fashion.com>
2023-03-02 22:05:03 +0100

    cleaning template + custom setup (#1)
    * modified config files with preferences
    
    * added @paths in tsconfig + folder structure + lint-staged
    
    * removed content from App.tsx

---
+++ main.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import App from "@components/App";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(<App />);
